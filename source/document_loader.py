92089f82005a3ad4c9c3f8597148c70c3ce508ce
Sarah Kim <sarahjaekim@berkeley.edu>
2026-07-16 16:50:02 -0700

    Finished document loader and plan for vector store

--- source/document_loader.py
+++ source/document_loader.py
@@ -7,6 +7,7 @@ from typing import List
 import fitz
 from langchain_core.documents import Document
 import pdfplumber
+import re
 
 
 
@@ -41,11 +42,35 @@ class PDFLoader:
         return documents
 
 class TableLoader:
-    #TODO
-    raise NotImplementedError
+    #loads table pdf text like employee, compensation tables
+    def __init__(self, filepath: str, doc_type: str = "generic", access_role: str = "employee", sorting_categories: list = None):
+        self.filepath = Path(filepath)
+        self.doc_type = doc_type
+        self.access_role = access_role
+        self.sorting_categories = sorting_categories
+        if not self.filepath.exists():
+            raise FileNotFoundError("File does not exist")
+    
+    def create_documents(self) -> List[dict]:
+        doc = fitz.open(self.filepath)
+        documents = []
+        first_page = doc[0].get_text()
+        first_line_stripped = first_page.splitlines()[0].strip()
+        categories = [c.strip() for c in first_line_stripped.split(",")]
+        self.sorting_categories = categories
+        for page in doc:
+            text = page.get_text()
+            split_by_id = re.split(r'(?=EMP)', text)
+            employees = split_by_id[1:]
+            for employee in employees:
+                emp = " ".join(employee.split())
+                employee_info = [data.strip() for data in emp.split(",")]
+                if len(employee_info) != len(self.sorting_categories):
+                    print(f"Length mismatch: expected {len(categories)} categories but received {len(employee_info)} categories.")
+                employee_dict = dict(zip(self.sorting_categories, employee_info))
+                documents.append(employee_dict)
+        return documents
 
-def store_access(filepath: str, doc_type: str = "generic", location: str = "universal"):
-    #TODO store access rules
 
 
 
--- source/vector_store.py
+++ source/vector_store.py
@@ -1,1 +1,3 @@
 #embeds and retrieves from chroma
+
+access_policy_rules = {} #TODO: log manually directly from manual
